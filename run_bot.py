70807345ec0fb35dc9c5960eea9b70c148d53d8d
kilfu0701 <kilfu0701@gmail.com>
2018-11-22 16:03:08 +0900

    fix starting env

--- run_bot.py
+++ run_bot.py
@@ -4,12 +4,16 @@ from bot import MercStoriaBot
 
 # botの接続と起動
 # （tokenにはbotアカウントのアクセストークンを入れてください）
-
 if __name__ == '__main__':
     env_config = None
-    with open('env.yaml', 'r') as stream:
-        env_config = yaml.load(stream)
+    token = None
+    try:
+        with open('env.yaml', 'r') as stream:
+            env_config = yaml.load(stream)
+            token = env_config['discord']['token']
+    except:
+        # this var can setup in https://dashboard.heroku.com > Settings > Config Vars
+        token = os.environ['DISCORD_BOT_TOKEN']
 
-    token = env_config['discord']['token']
     bot = MercStoriaBot()
     bot.run(token)
